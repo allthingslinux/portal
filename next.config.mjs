bb8d227599193fb06a20528e2f1458b6c99d7c2c
kzndotsh <admin@kaizen.wtf>
2024-12-06 15:29:29 -0500

    feat(next.config.mjs): add configuration options for reactStrictMode, poweredByHeader, pageExtensions, and eslint to enhance development experience and build process
    docs(next.config.mjs): add @ts-check for type checking and a TODO comment for reactStrictMode to indicate future changes needed for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // TODO: Remove this when pushing to production
+  reactStrictMode: false,
+  poweredByHeader: false,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
