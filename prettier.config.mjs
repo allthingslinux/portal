d875ecce162fb39148df66be224244d7aa960a29
kzndotsh <admin@kaizen.wtf>
2024-12-06 15:29:39 -0500

    chore(prettier): add Prettier configuration file for consistent code formatting across the project

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+};
+
+export default config;
