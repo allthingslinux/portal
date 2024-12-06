43ebbca1125d55a7ad6b8e45cd7b9551f4aff6bd
kzndotsh <admin@kaizen.wtf>
2024-12-06 15:28:57 -0500

    chore(eslint): migrate ESLint configuration to eslint.config.mjs for module support and enhanced configuration options

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
