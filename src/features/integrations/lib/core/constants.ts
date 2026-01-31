40c173e995359172400fd76ae8769f37dcfdc3b0
Logan Honeycutt <admin@kzn.sh>
2026-01-31 14:54:51 -0500

    feat(constants.ts): add 'Pending' status to integrationStatusLabels
    Introduce a 'Pending' status to the integrationStatusLabels
    to accommodate scenarios where an integration is not yet active
    but is in the process of being set up or awaiting approval. This
    enhancement provides a more comprehensive representation of
    integration states.

--- src/features/integrations/lib/core/constants.ts
+++ src/features/integrations/lib/core/constants.ts
@@ -2,6 +2,7 @@ import type { IntegrationStatus } from "@/shared/utils/constants";
 
 export const integrationStatusLabels: Record<IntegrationStatus, string> = {
   active: "Active",
+  pending: "Pending",
   suspended: "Suspended",
   deleted: "Deleted",
 };
