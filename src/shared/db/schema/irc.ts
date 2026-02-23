f7cfc0be682d17294a420c6e64ee278a7c3513b1
Logan Honeycutt <admin@kzn.sh>
2026-02-22 19:17:58 -0500

    fix(db): update import path for createInsertSchema and createSelectSchema from drizzle-zod to drizzle-orm/zod

--- src/shared/db/schema/irc.ts
+++ src/shared/db/schema/irc.ts
@@ -9,7 +9,7 @@ import {
   timestamp,
   uniqueIndex,
 } from "drizzle-orm/pg-core";
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { createInsertSchema, createSelectSchema } from "drizzle-orm/zod";
 
 import { user } from "./auth";
 
