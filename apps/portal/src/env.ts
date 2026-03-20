5e2559ce69962b303cd8049c46683347085ff173
Logan Honeycutt <admin@kzn.sh>
2026-03-19 20:32:13 -0400

    chore: update apps/portal/src/env.ts

--- apps/portal/src/env.ts
+++ apps/portal/src/env.ts
@@ -3,6 +3,7 @@ import { keys as observability } from "@portal/observability/keys";
 import { createEnv } from "@t3-oss/env-nextjs";
 
 import { keys as auth } from "@/auth/keys";
+import { keys as bridge } from "@/features/integrations/lib/bridge/keys";
 import { keys as discord } from "@/features/integrations/lib/discord/keys";
 import { keys as irc } from "@/features/integrations/lib/irc/keys";
 import { keys as mailcow } from "@/features/integrations/lib/mailcow/keys";
@@ -14,6 +15,7 @@ import { keys as devTools } from "@/shared/dev-tools/keys";
 export const env = createEnv({
   extends: [
     auth(),
+    bridge(),
     changelog(),
     database(),
     devTools(),
