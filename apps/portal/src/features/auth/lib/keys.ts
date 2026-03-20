abd75946933ddd9a90ef7ead9ae2648603d5065a
Logan Honeycutt <admin@kzn.sh>
2026-03-19 20:32:23 -0400

    chore: update apps/portal/src/features/auth/lib/keys.ts

--- apps/portal/src/features/auth/lib/keys.ts
+++ apps/portal/src/features/auth/lib/keys.ts
@@ -6,9 +6,13 @@ export const keys = () =>
     server: {
       BETTER_AUTH_SECRET: z.string().min(1).optional(),
       BETTER_AUTH_URL: z.url().optional(),
+      DISCORD_CLIENT_ID: z.string().min(1).optional(),
+      DISCORD_CLIENT_SECRET: z.string().min(1).optional(),
     },
     runtimeEnv: {
       BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
       BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+      DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+      DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     },
   });
